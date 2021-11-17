02097ad63f3f02551aa206761de189ceecb7b038
Matias <mpintov42@gmail.com>
2021-11-16 21:31:16 -0300

    Ultimos arreglos, solo falta arreglar el footer en version mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("#navoption").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default
-    // behavior
-    if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-    // Store hash
-    var hash = this.hash;
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds
-    // it takes to scroll to the specified area
-    $('html, body').animate({
-    scrollTop: $(hash).offset().top
-    }, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click
-        // behavior)
-        window.location.hash = hash;
-        });
-        } // End if
+
+
+$(function(){
+
+        $("#redessociales").click(function(){
+            alert("Lamentablemente nuestras redes sociales no están funcionando :(");
+         })
+
+        // Es dificil definir el bottom de una seccion con jquery asi que esto lo calcula
+
+        var bottom = $('#photo').position().top + $('#photo').outerHeight(true);
+       
+        // El navbar transparente en todo el sitio se mezclaba con las letras así que mejor le agregué color una vez que se pasa del carousel
+        $(window).scroll(function() {
+            if ($("#menu").offset().top > bottom){
+                $("#menu").removeClass("bg-transparent").addClass("bg-black");
+            } 
+            else{
+                $("#menu").removeClass("bg-black").addClass("bg-transparent");
+            }
+        
+            });
     });
-});
